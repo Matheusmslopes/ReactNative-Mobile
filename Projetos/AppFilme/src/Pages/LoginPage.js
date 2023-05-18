0030653a3deccf99a60d93e8ad9b023d20efe4c3
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-05-18 15:12:41 -0300

    Update LoginPage.js

--- Projetos/AppFilme/src/Pages/LoginPage.js
+++ Projetos/AppFilme/src/Pages/LoginPage.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Link } from '@react-navigation/native';
+import Login from "../Components/Login";
+
+
+
 
 export default function LoginPage(){
+
     return (
         <View style = {styles.container}>
-            <Text style = {styles.login}>Pagina de login</Text>
-            <Link to= {{screen: "RegisterPage"}} style = {styles.link}>Registre-se!</Link>
+            <Image style = {styles.img} source = {{uri: 'https://i.pinimg.com/736x/fa/b0/f4/fab0f4c853e2305018eb07e5e3034341.jpg'}}/>
+            <Text style = {styles.txt}>Alguma coisa Aqui?</Text>
+            <Login/>
+            <Link to= {{screen: "Página de registro"}} style = {styles.link}>Entrar</Link>
+            <Link to= {{screen: "Página de registro"}} style = {styles.link}>Registre-se!</Link>
+           
         </View>
     )
 }
@@ -13,15 +22,31 @@ export default function LoginPage(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black'
+        backgroundColor: "#0D0D0D",
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 
-    login: {
-        color: 'white'
+    img: {
+        height: 200,
+        width: 200,
+        borderRadius: 20,
+    },
+
+    txt: {
+        color:"#088C7F",
+        fontSize: 40,
+        marginTop: 15
     },
 
     link: {
-        backgroundColor: 'blue',
-        textAlign: 'center'
-    }
+        backgroundColor: "#088C7F",
+        color: '#F2CB05',
+        width: 160,
+        height: 40,
+        borderRadius: 20,
+        textAlign: 'center',
+        justifyContent: 'center',
+        marginBottom: 10
+    },
 })
